0e8f2255e662a1884c8d2997bf94e62c0f0300e5
wafy <zyalwfie@gmail.com>
2025-05-05 02:41:35 +0800

    feat: structured dir

---
+++ src/components/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import styles from './Home.module.css';
+
+const Home = () => {
+	return (
+		<>
+			<main className={styles.home}>
+				<h1>Home Page!</h1>
+			</main>
+		</>
+	);
+};
+
+export default Home;
